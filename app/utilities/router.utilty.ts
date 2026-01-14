96a54151542aac8ff3a16f7a79432c24c6c0138c
Gabbar <gabbar.silver@gmail.com>
2026-01-14 19:57:45 +0530

    feat: toast response

--- app/utilities/router.utilty.ts
+++ app/utilities/router.utilty.ts
@@ -2,6 +2,7 @@ import { auth } from "~/lib/auth.server";
 import { ValidationError } from "./errors/validation.errors";
 import { data } from "react-router";
 import { type ApiResponse } from "./types/api.types";
+import { toast } from "sonner";
 
 export async function authorizeRequest(request: Request, allowedMethod?: HttpMethod, allowedRole?: number) {
     // Method Check
@@ -26,4 +27,9 @@ export function requireHttpMethod(request: Request, allowedMethod: HttpMethod) {
             success: false,
             error: { message: "Invalid method please try again!" },
         }, { status: 404 });
+}
+
+export function toastResponse(data: ApiResponse) {
+    if (!data.success) toast.error(data.error.message || "An error occurred. Please try again!");
+    else if (data.message) toast.success(data.message);
 }
